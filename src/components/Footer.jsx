78e523ab480c6ab3261f516f7cc283d331fc857e
estivedelgado16 <138074670+estivedelgado16@users.noreply.github.com>
2024-03-01 18:14:47 -0500

    update styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,10 +13,10 @@ function Footer(){
                 <a href="https://github.com/" target="_blank">
                     <img className="gitHub" src={GitHub} alt="github"/>
                 </a>
-                <a href="">
+                <a href="https://twitter.com/?lang=es" target='_blank'>
                     <img className='twitter' src={Twitter} alt="twitter" />
                 </a>
-                <a href="">
+                <a href="https://www.instagram.com/" target='_blank'>
                     <img className="instagram" src={Instagram} alt="instagram" />
                 </a>
         </div>
